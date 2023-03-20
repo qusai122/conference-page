ed139d200521426c48bc3e588842ad97944712d4
qusai122 <qusai.2010.123@gmail.com>
2023-03-20 18:09:56 +0200

    Finish first page for mobile

--- index.js
+++ index.js
@@ -28,66 +28,63 @@ RemoveMenu(closeNav);
 RemoveMenu(choose);
 // programe data
 
-
 const programs = [
-    {
-        title: 'BMW M3',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'assits/second-car.jpg'
-    },
-    {
-        title: 'BMW M4',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'assits/second-car.jpg'
-    },
-    {
-        title: 'BMW M5',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'assits/second-car.jpg'
-    },
-    {
-        title: 'ALL NEW M8',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'assits/second-car.jpg'
-    },
+  {
+    title: 'BMW M3',
+    message: 'Listen to the speakers from various countries.',
+    icon: 'assits/second-car.jpg',
+  },
+  {
+    title: 'BMW M4',
+    message: 'Listen to the speakers from various countries.',
+    icon: 'assits/second-car.jpg',
+  },
+  {
+    title: 'BMW M5',
+    message: 'Listen to the speakers from various countries.',
+    icon: 'assits/second-car.jpg',
+  },
+  {
+    title: 'NEW M8',
+    message: 'Listen to the speakers from various countries.',
+    icon: 'assits/second-car.jpg',
+  },
 
-]
+];
 
-
-//Creat main programes
+// Creat main programes
 document.querySelector('.program-holder').innerHTML = programs.map((program) => `
     <div class="container p-2 ">
         <div class="d-flex align-items-center program-card p-2">
-            <img src=${program.icon} width="55" height="55" alt="lecture">
+            <img src=${program.icon} width="50px" height="50px" alt="lecture">
             <h4 class="program-title px-2">${program.title}</h4>
             <span class="px-1 text-white">${program.message}</span>
         </div>
     </div>
 `).join('');
 
-
 const speakers = [
-    {
-        name: 'Oliver Zipse',
-        about: 'Chairman of the Board of Management',
-        quote: '',
-        image: 'assits/speaker1.jpg'
-    },
-    {
-        name: 'ILKA HORSTMEIER',
-        about: 'People and Real Estate, Labour Relations Director',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'assits/speaker2.jpg'
-    },
-    {
-        name: 'DR. MILAN NEDELJKOVIĆ',
-        about: 'Production',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'assits/speaker3.jpg'
-    },
-]
+  {
+    name: 'Oliver Zipse',
+    about: 'Chairman of the Board of Management',
+    quote: 'He was Member of the Board of Management of BMW AG, Production',
+    image: 'assits/speaker1.jpg',
+  },
+  {
+    name: 'ILKA HORSTMEIER',
+    about: 'People and Real Estate, Labour Relations Director',
+    quote: 'Member of the Board of Management of BMW AG, People and Real Estate, Labour Relations Director',
+    image: 'assits/speaker2.jpg',
+  },
+  {
+    name: 'DR. MILAN NEDELJKOVIĆ',
+    about: 'Production',
+    quote: 'Member of the Board of Management of BMW AG, Production',
+    image: 'assits/speaker3.jpg',
+  },
+];
 // console.log(speakers)
-document.querySelector('.speaker-holder').innerHTML =  speakers.map( speak => `
+document.querySelector('.speaker-holder').innerHTML = speakers.map((speak) => `
 <div class="container speaker-card mt-4">
 <div class="image-holder">
     <img class = "speaker-img" src=${speak.image} width="120" height="90%" alt="speaker 1">
@@ -98,4 +95,4 @@ document.querySelector('.speaker-holder').innerHTML =  speakers.map( speak => `
     <hr class="line-breaker">
     <p class="speaker-quote">${speak.quote}</p>
 </div>
-</div>`).join('')
+</div>`).join('');

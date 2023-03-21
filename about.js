9c6dde662ed8879d8c1013db9872c5a4dbe55611
Qusai Issa <qusai.2010.123@hotmail.com>
2023-03-21 02:18:49 +0200

    Fix linters and connect pages

--- about.js
+++ about.js
@@ -1,21 +1,20 @@
-
 // Past summits
 
 const summits = [
-    {
-      year: '2015',
-      message: 'CG Global summit 2015 in Berlin',
-      background: 'assits/summit1.jpg',
-    },
-    {
-      year: '2020',
-      message: 'CG Global summit 2020 in Dubai',
-      background: 'assits/summit2.jpg',
-    },
-  ];
-  
-  // Creat summits html
-  document.querySelector('.summits-holder').innerHTML = summits.map((summit) => `
+  {
+    year: '2015',
+    message: 'CG Global summit 2015 in Berlin',
+    background: 'assits/summit1.jpg',
+  },
+  {
+    year: '2020',
+    message: 'CG Global summit 2020 in Dubai',
+    background: 'assits/summit2.jpg',
+  },
+];
+
+// Creat summits html
+document.querySelector('.summits-holder').innerHTML = summits.map((summit) => `
       <div class="container p-2 ">
           <div class="d-flex align-items-center program-card p-2">
               <img class= "prog-icon" src=${summit.background} width="50px" height="50px" alt="lecture">

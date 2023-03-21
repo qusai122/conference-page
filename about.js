10704f890bc79e93e2a43feffb5e93617e99a531
qusai122 <qusai.2010.123@gmail.com>
2023-03-21 13:40:20 +0200

    Final touchs

--- about.js
+++ about.js
@@ -16,10 +16,9 @@ const summits = [
 // Creat summits html
 document.querySelector('.summits-holder').innerHTML = summits.map((summit) => `
       <div class="container p-2 ">
-          <div class="d-flex align-items-center program-card p-2">
-              <img class= "prog-icon" src=${summit.background} width="50px" height="50px" alt="lecture">
-              <h4 class="program-title px-2">${summit.year}</h4>
-              <span class="px-1 text-white">${summit.message}</span>
+          <div class="d-flex align-items-center summit-card p-2" style= "background-image: url(${summit.background});">
+              <h4 class="summit-year px-2">${summit.year}</h4>
+              <span class="px-1 summit-message">${summit.message}</span>
           </div>
       </div>
   `).join('');
